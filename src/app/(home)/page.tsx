a0e96073a303f7246e745eac9cc054ce6dcf370d
Vinicius <vinicius.benfica.ruy@gmail.com>
2023-11-14 14:54:22 -0300

    fix home page build

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,7 +2,7 @@ import { url } from '@/utils/url';
 import { IProducts } from '@/types/IProduct';
 import Body from './sections/body/body';
 
-export async function getAllProducts(): Promise<IProducts> {
+async function getAllProducts(): Promise<IProducts> {
   try {
     const response = await fetch(
       `${url}/products?page=1&rows=8&sortBy=id&orderBy=DESC`,
